1fc4773da26073d785a78f65d4ad11ae1323dc93
Jennifer Moundanos <jmoundanos@gmail.com>
2020-10-22 21:57:30 -0500

    display cards

--- src/TinderCard.js
+++ src/TinderCard.js
@@ -4,19 +4,19 @@ function TinderCard() {
     const [people, setPeople] = useState([
         {
         name: "Lizzo",
-        url: "https://www.popsugar.com/celebrity/lizzo-fun-facts-47422795"
+        url: "https://i.insider.com/5e00e392954bda060200fdd4?width=1700&format=jpeg&auto=webp"
         },
         {
         name: "Mariah Carey",
-        url: "https://en.wikipedia.org/wiki/Mariah_Carey#/media/File:Mariah_Carey_WBLS_2018_Interview_2.jpg",
-        }
+        url: "https://scontent.faus1-1.fna.fbcdn.net/v/t1.0-9/p960x960/109086844_10158549197207766_4561136586739777109_o.jpg?_nc_cat=1&ccb=2&_nc_sid=85a577&_nc_ohc=rDBHXtvZULoAX_0i_ww&_nc_ht=scontent.faus1-1.fna&tp=6&oh=30ec1993dd3005ed4ca4b86df87be567&oe=5FB7C335",
+        },
 
     ]);
     return (
         <div>
             <h1>Tinder Cards</h1> 
-            {people.map(person => (
-                <TinderCard>
+            {people.map((person) => (
+                <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
                     <div style={{ backgroundImage: 'url(${person.url}}' }} className="card">
                     <h3>{person.name}</h3>
                     </div>
